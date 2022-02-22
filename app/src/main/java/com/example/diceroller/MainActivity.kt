7734bea9ce637165df5fdf64aa2d486f72c5db26
romanboyarkin <boyra75.rb@gmail.com>
2022-02-22 22:49:17 +0900

    correct main activity

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImage: ImageView
@@ -21,23 +22,17 @@ class MainActivity : AppCompatActivity() {
         // Set a click listener on the button to roll the dice when the user taps the button
         rollButton.setOnClickListener { rollDice() }
 
-        // Do a dice roll when the app starts
-        rollDice()
+        diceImage = findViewById(R.id.imageView)
+
     }
+
 
     /**
      * Roll the dice and update the screen with the result.
      */
     private fun rollDice() {
-        // Create new Dice object with 6 sides and roll it
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
-
-        // Find the ImageView in the layout
-        val diceImage: ImageView = findViewById(R.id.imageView)
-
-        // Determine which drawable resource ID to use based on the dice roll
-        val drawableResource = when (diceRoll) {
+        val randomInt = Random.nextInt(6) + 1
+        val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -46,23 +41,6 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        // Update the ImageView with the correct drawable resource ID
         diceImage.setImageResource(drawableResource)
-
-        // Update the content description
-        diceImage.contentDescription = diceRoll.toString()
-    }
-}
-
-/**
- * Dice with a fixed number of sides.
- */
-class Dice(private val numSides: Int) {
-
-    /**
-     * Do a random dice roll and return the result.
-     */
-    fun roll(): Int {
-        return (1..numSides).random()
     }
 }
